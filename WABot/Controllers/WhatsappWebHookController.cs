aa27a9fb3cb9250e344602d489229b6371b639bd
stemensooh <stemensooh@gmail.com>
2021-11-28 21:55:39 -0500

    integracion de Twilio

--- WABot/Controllers/WhatsappWebHookController.cs
+++ WABot/Controllers/WhatsappWebHookController.cs
@@ -2,10 +2,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using static Twilio.Rest.Api.V2010.Account.Call.FeedbackSummaryResource;
 
 namespace WABot.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class WhatsappWebHookController : ControllerBase
     {
@@ -17,5 +23,41 @@ namespace WABot.Controllers
             this.configuration = configuration;
             this.logger = logger;
         }
+
+        public string Get()
+        {
+            return "Running...";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(RequestMessage request)
+        {
+            TwilioClient.Init(configuration["Twilio:AccountSid"], configuration["Twilio:AuthToken"]);
+
+            var messageOptions = new CreateMessageOptions(new PhoneNumber($"whatsapp:{request.NumberPhone}"))
+            {
+                From = new PhoneNumber($"whatsapp:{configuration["Twilio:From"]}"),
+                Body = request.Message
+            };
+
+            var message = await MessageResource.CreateAsync(messageOptions);
+            if (message.Status == StatusEnum.Completed)
+            {
+                
+            }
+
+            return Ok(message);
+
+        }
+
+    }
+
+    public class RequestMessage
+    {
+        [JsonProperty("numberPhone")]
+        public string NumberPhone { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
     }
 }
